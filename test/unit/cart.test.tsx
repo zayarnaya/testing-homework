bad682ee04073260fbaf2f6d01d3be0957fa85ed
Veronika Zayarnaya <veronika.zayarnaya@yandex.ru>
2023-07-01 21:22:44 +0300

    какой-то мок работает

--- test/unit/cart.test.tsx
+++ test/unit/cart.test.tsx
@@ -1,178 +1,84 @@
-// import React from 'react';
+import { it, jest, expect } from "@jest/globals";
 
-// import { fireEvent, getByLabelText, getByText, render } from '@testing-library/react';
-// import { Cart } from '../../src/client/pages/Cart';
-// import configureMockStore from "redux-mock-store"; 
-// import { Provider } from 'react-redux';
-// import { ApplicationState } from '../../src/client/store';
+import { render } from "@testing-library/react";
+import events from "@testing-library/user-event";
+import { BrowserRouter } from 'react-router-dom';
 
 
-
-// const empty='';
-// const name = 'ыыы';
-// const phoneWrong='шыплоыир';
-// const phoneRight = '8916101101';
-
-// // здесь стейт какой-то ннада
-
-// const mockStore = configureMockStore([]); 
-
-// const initialState: ApplicationState =  {
-//     details: {},
-//     cart: {
-//         1: {
-//             name: 'fake1',
-//             price: 87,
-//             count: 1,
-//         }
-//     }
-// }
-// // не, чот не то
-// describe("Корзина", () => { 
-//     it("если в форму все введено корректно, должно появляться уведомление с классом SuccessMessage и alert-success", () => { 
-//       const store = mockStore(initialState); 
-//       const { getByText, getByLabelText } = render( 
-//         <Provider store={store}> 
-//             <Cart />
-//         </Provider> 
-//       ); 
-
-//       const button = getByText("Checkout"); 
-//       const nameInput = getByLabelText('Name') as HTMLInputElement;
-//       fireEvent.change(nameInput, { target: { value: name}});
-//       const phoneInput = getByLabelText('Phone') as HTMLInputElement;
-//       fireEvent.change(phoneInput, { target: { value: phoneRight}});
-//       const addressInput = getByLabelText('Address') as HTMLInputElement;
-//       fireEvent.change(addressInput, { target: { value: empty}});
-
-//       fireEvent.click(button); 
-//       const actions = store.getActions(); 
-//       expect(actions).toEqual([{ type: 'CHECKOUT' }]); 
-//     }); 
-//   }); 
-
-// // describe('Корзина', () => {
-// //     it('если в форму все введено корректно, должно появляться уведомление с классом SuccessMessage и alert-success', () => {
-// //         const form = <Cart />;
-
-// //         const { container, getByRole, getByLabelText } = render(form);
+import { Application } from '../../src/client/Application';
 
 
-// //         const button = getByRole("button");
-// //         const nameInput = getByLabelText('Name') as HTMLInputElement;
-// //         fireEvent.change(nameInput, { target: { value: empty}})
-// //         fireEvent.click(button);
-// //         expect(nameInput.classList.contains('is-invalid')).toBe(true); 
+import { MemoryRouter } from "react-router";
+import { initStore } from "../../src/client/store";
+import { Provider } from "react-redux";
+import { CartApi, ExampleApi } from "../../src/client/api";
+import React from 'react'
+import { Cart } from "../../src/client/pages/Cart";
+import { CartState } from "../../src/common/types";
+// import { dummyCart } from "./helpers";
+jest.mock('../../src/client/api');
+jest.mocked(CartApi).mockImplementation(() => {
+  return {getState: jest.fn<() => CartState>().mockReturnValue(dummyCart), setState: jest.fn()}
+});
+const dummyCart = {
+      1: {
+          name: 'Fake#1',
+          price: 12,
+          count: 1,
+      },
+      2: {
+          name: 'Fake#2',
+          price: 56,
+          count: 3,
+      },
+      3: {
+          name: 'Fake#3',
+          price: 86,
+          count: 2,
+      }
+  }
+it('по адресу /about должна открываться страница "о проекте"', () => {
+  const basename = 'http://localhost:3000/hw/store';
+  // const mockGetState = jest.fn().mockReturnValue(dummyCart);
 
-// //     });
-// // });
+  const cart = new CartApi;
 
-// // describe('Форма заказа', () => {
-// //     it('если имя введено, проверка не должна присваивать is-invalid', () => {
-// //         const form = <Form onSubmit={() => console.log('НУ САБМИТ')}/>;
-
-// //         const { container, getByRole, getByLabelText } = render(form);
-
-
-// //         const button = getByRole("button");
-// //         const nameInput = getByLabelText('Name') as HTMLInputElement;
-// //         fireEvent.change(nameInput, { target: { value: name}})
-// //         // console.log(nameInput);
-// //         fireEvent.click(button);
-// //         expect(nameInput.classList.contains('is-invalid')).toBe(false); // падает, супер
-// //         // expect(container.querySelector('.invalid-feedback')?.textContent).toBe('Please provide your name');
-// //         // console.log(container.outerHTML);
-// //         // console.log(container.innerHTML);
-
-// //         // expect(container.textContent).toBe('example');
-// //     });
-// // });
-
-
-// // describe('Форма заказа', () => {
-// //     it('если в поле телефона введены буквы, должна срабатывать проверка', () => {
-// //         const form = <Form onSubmit={() => console.log('НУ САБМИТ')}/>;
-
-// //         const { container, getByRole, getByLabelText } = render(form);
-
-
-// //         const button = getByRole("button");
-// //         const phoneInput = getByLabelText('Phone') as HTMLInputElement;
-// //         fireEvent.change(phoneInput, { target: { value: phoneWrong}})
-// //         // console.log(nameInput);
-// //         fireEvent.click(button);
-// //         expect(phoneInput.classList.contains('is-invalid')).toBe(true); // падает, супер
-// //         // expect(container.querySelector('.invalid-feedback')?.textContent).toBe('Please provide your name');
-// //         // console.log(container.outerHTML);
-// //         // console.log(container.innerHTML);
-
-// //         // expect(container.textContent).toBe('example');
-// //     });
-// // });
+  const api = new ExampleApi(basename);
+  // const cart = new CartApi();
+  const store = initStore(api, cart);
+  
+  const cartPage = (
+<MemoryRouter initialEntries={["/about"]} initialIndex={0}>
 
 
-// // describe('Форма заказа', () => {
-// //     it('если в поле телефона введен правильный формат номера, проверка не должна выставлять is-invalid', () => {
-// //         const form = <Form onSubmit={() => console.log('НУ САБМИТ')}/>;
+          <Provider store={store}>
+              <Cart />
+          </Provider>
+          </MemoryRouter>
+  );
+  const { container } = render(cartPage);
+  console.log(container.innerHTML)
 
-// //         const { container, getByRole, getByLabelText } = render(form);
+  // expect(getByTestId("page-title").textContent).toEqual("About");
+});
 
+// it("если добавить элемент, он появляется в списке", async () => {
+//   const store = initStore();
+//   const application = (
+//     <BrowserRouter>
+//       <Provider store={store}>
+//         <Application />
+//       </Provider>
+//     </BrowserRouter>
+//   );
 
-// //         const button = getByRole("button");
-// //         const phoneInput = getByLabelText('Phone') as HTMLInputElement;
-// //         fireEvent.change(phoneInput, { target: { value: phoneRight}})
-// //         // console.log(nameInput);
-// //         fireEvent.click(button);
-// //         expect(phoneInput.classList.contains('is-invalid')).toBe(false); // падает, супер
-// //         // expect(container.querySelector('.invalid-feedback')?.textContent).toBe('Please provide your name');
-// //         // console.log(container.outerHTML);
-// //         // console.log(container.innerHTML);
+//   const { getByTestId, getAllByTestId } = render(application);
 
-// //         // expect(container.textContent).toBe('example');
-// //     });
-// // });
+//   await events.type(getByTestId("input-add"), "Сделать домашку");
 
+//   await events.click(getByTestId("button-add"));
 
+//   const items = getAllByTestId("list-item");
 
-// // describe('Форма заказа', () => {
-// //     it('если в поле адреса пусто, должна срабатывать проверка', () => {
-// //         const form = <Form onSubmit={() => console.log('НУ САБМИТ')}/>;
-
-// //         const { container, getByRole, getByLabelText } = render(form);
-
-
-// //         const button = getByRole("button");
-// //         const addressInput = getByLabelText('Address') as HTMLInputElement;
-// //         fireEvent.change(addressInput, { target: { value: empty}})
-// //         // console.log(nameInput);
-// //         fireEvent.click(button);
-// //         expect(addressInput.classList.contains('is-invalid')).toBe(true); // падает, супер
-// //         // expect(container.querySelector('.invalid-feedback')?.textContent).toBe('Please provide your name');
-// //         // console.log(container.outerHTML);
-// //         // console.log(container.innerHTML);
-
-// //         // expect(container.textContent).toBe('example');
-// //     });
-// // });
-
-
-// // describe('Форма заказа', () => {
-// //     it('если в поле адреса не пусто, проверка не должна выставлять is-invalid', () => {
-// //         const form = <Form onSubmit={() => console.log('НУ САБМИТ')}/>;
-
-// //         const { container, getByRole, getByLabelText } = render(form);
-
-
-// //         const button = getByRole("button");
-// //         const addressInput = getByLabelText('Address') as HTMLInputElement;
-// //         fireEvent.change(addressInput, { target: { value: name}})
-// //         // console.log(nameInput);
-// //         fireEvent.click(button);
-// //         expect(addressInput.classList.contains('is-invalid')).toBe(false); // падает, супер
-// //         // expect(container.querySelector('.invalid-feedback')?.textContent).toBe('Please provide your name');
-// //         // console.log(container.outerHTML);
-// //         // console.log(container.innerHTML);
-
-// //         // expect(container.textContent).toBe('example');
-// //     });
-// // });
+//   expect(items.map((el) => el.textContent)).toContain("Сделать домашку");
+// });
--- test/unit/helpers.ts
+++ test/unit/helpers.ts
@@ -4,7 +4,7 @@
 // import { initStore } from '../../src/client/store';
 // import { Provider } from 'react-redux'
 // import { PreloadedState } from 'redux';
-// import { ExampleApi } from '../../src/client/api';
+// import { ExampleApi, CartApi } from '../../src/client/api';
 // import axios from 'axios';
 // import {jest} from '@jest/globals';
 // import { CartState, CheckoutFormData, CheckoutResponse, Product, ProductShortInfo } from '../../src/common/types';
@@ -14,91 +14,113 @@
 // //     store?: AppStore
 // //   }
 
-// const mockGetProducts = jest.fn<() => ProductShortInfo[]>();
-// jest.mocked(ExampleApi).mockImplementation(() => {
-//   return {
-//     getProducts: mockGetProducts,
-//   };
-// });
+// // const mockGetProducts = jest.fn<() => ProductShortInfo[]>();
+// // jest.mocked(ExampleApi).mockImplementation(('') => {
+// //   return {
+// //     getProducts: mockGetProducts,
+// //   };
+// // });
 
 
-// export function renderWithProviders(  ui: React.ReactElement,
-//     {
-//       preloadedState = {},
-//       // Automatically create a store instance if no store was passed in
-//       store = initStore(mockExampleApi, mockCartApi),
-//       ...renderOptions
-//     } = {}
-//   ) {
-//     function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
-//       return <Provider store={store}>{children}</Provider>
-//     }
+// // export function renderWithProviders(  ui: React.ReactElement,
+// //     {
+// //       preloadedState = {},
+// //       // Automatically create a store instance if no store was passed in
+// //       store = initStore(mockExampleApi, mockCartApi),
+// //       ...renderOptions
+// //     } = {}
+// //   ) {
+// //     function Wrapper({ children }: PropsWithChildren<{}>): JSX.Element {
+// //       return <Provider store={store}>{children}</Provider>
+// //     }
   
-//     // Return an object with the store and all of RTL's query functions
-//     return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
-//   }
+// //     // Return an object with the store and all of RTL's query functions
+// //     return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) }
+// //   }
 
-//   const mockApi = new ExampleApi('defg');
-//   mockApi.getProducts = () => {
-//     return [
-//         {
-//             id: 123,
-//             name: 'Fake#1',
-//             price: 12,
-//         },
-//         {
-//             id: 1,
-//             name: 'Fake#2',
-//             price: 122,
-//         },
-//         {
-//             id: 33,
-//             name: 'Fake#3',
-//             price: 458,
-//         },
+// //   const mockApi = new ExampleApi('defg');
+// //   mockApi.getProducts = () => {
+// //     return [
+// //         {
+// //             id: 123,
+// //             name: 'Fake#1',
+// //             price: 12,
+// //         },
+// //         {
+// //             id: 1,
+// //             name: 'Fake#2',
+// //             price: 122,
+// //         },
+// //         {
+// //             id: 33,
+// //             name: 'Fake#3',
+// //             price: 458,
+// //         },
 
-//     ]
-//   }
+// //     ]
+// //   }
 
-//   export class mockExampleApi extends ExampleApi {
-//     constructor() {
-//         super('fggs');
-//         this.getProducts = () => {
-//             // return await axios.get<ProductShortInfo[]>(`${this.basename}/api/products`);
-//             return [
-//                 {
-//                     id: 123,
-//                     name: 'Fake#1',
-//                     price: 12,
-//                 },
-//                 {
-//                     id: 1,
-//                     name: 'Fake#2',
-//                     price: 122,
-//                 },
-//                 {
-//                     id: 33,
-//                     name: 'Fake#3',
-//                     price: 458,
-//                 },
+// //   export class mockExampleApi extends ExampleApi {
+// //     constructor() {
+// //         super('fggs');
+// //         this.getProducts = () => {
+// //             // return await axios.get<ProductShortInfo[]>(`${this.basename}/api/products`);
+// //             return [
+// //                 {
+// //                     id: 123,
+// //                     name: 'Fake#1',
+// //                     price: 12,
+// //                 },
+// //                 {
+// //                     id: 1,
+// //                     name: 'Fake#2',
+// //                     price: 122,
+// //                 },
+// //                 {
+// //                     id: 33,
+// //                     name: 'Fake#3',
+// //                     price: 458,
+// //                 },
     
-//             ]
-//         }
-//     }
+// //             ]
+// //         }
+// //     }
 
     
 
-//     async getProductById(id: number) {
-//         return await axios.get<Product>(`${this.basename}/api/products/${id}`);
-//     }
+// //     async getProductById(id: number) {
+// //         return await axios.get<Product>(`${this.basename}/api/products/${id}`);
+// //     }
 
-//     async checkout(form: CheckoutFormData, cart: CartState) {
-//         return await axios.post<CheckoutResponse>(`${this.basename}/api/checkout`, { form, cart });
+// //     async checkout(form: CheckoutFormData, cart: CartState) {
+// //         return await axios.post<CheckoutResponse>(`${this.basename}/api/checkout`, { form, cart });
+// //     }
+// // }
+// export const dummyCart = {
+//     1: {
+//         name: 'Fake#1',
+//         price: 12,
+//         count: 1,
+//     },
+//     2: {
+//         name: 'Fake#2',
+//         price: 56,
+//         count: 3,
+//     },
+//     3: {
+//         name: 'Fake#3',
+//         price: 86,
+//         count: 2,
 //     }
 // }
 
 // export const LOCAL_STORAGE_CART_KEY = 'example-store-cart';
-
+// const mockGetState = jest.fn<() => CartState>().mockReturnValue(dummyCart);
+// jest.mocked(CartApi).mockImplementation(() => {
+//     return {
+//         ...mockGetState,
+//     }
+// });
 // export class mockCartApi {
 //     getState(): CartState {
 //         try {
@@ -113,3 +135,13 @@
 //         localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cart));
 //     }
 // }
+
+// // export interface CartItem {
+// //     name: string;
+// //     price: number;
+// //     count: number;
+// // }
+
+// // export type CartState = Record<number, CartItem>;
+
+
